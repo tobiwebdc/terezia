11a7a4737621e978395850add98e0bda1c2cc903
tobiczech <tobiczech@gmail.com>
2026-08-14 18:17:30 +0200

    eventy update logic

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,35 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/css/output.css");
-  eleventyConfig.addPassthroughCopy("src/js");
-  eleventyConfig.addPassthroughCopy("src/images");
-  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   eleventyConfig.addGlobalData("currentYear", () => new Date().getFullYear());
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd LLL yyyy");
+  });
+
+  eleventyConfig.addShortcode("currentYear", () => `${new Date().getFullYear()}`);
+
+  // Products: hide anything explicitly marked visible: false, newest/A-Z first
+  eleventyConfig.addCollection("products", (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob("src/content/products/*.md")
+      .filter((item) => item.data.visible !== false)
+      .sort((a, b) => (a.data.name || "").localeCompare(b.data.name || ""));
+  });
+
+  eleventyConfig.addCollection("pages", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("src/content/pages/*.md");
+  });
 
   return {
     dir: {
       input: "src",
       output: "_site",
+      includes: "_includes",
+      data: "_data",
     },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    templateFormats: ["njk", "md", "11ty.js"],
   };
 };
